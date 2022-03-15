a6c9c1bc9be9bdcfbe8f54c41b653a7a0e8bef39
way99 <745855589@qq.com>
2022-03-15 18:56:08 +0800

    feat:3-2添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import * as qs from 'qs';
+import { cleanObject } from 'utils';
+import { List } from './list';
+import { SearchPanel } from './search-panel';
+
+
+const apiUrl=process.env.REACT_APP_API_URL
+
+export const ProjectListScreen=()=>{
+    //负责人
+    const [users, setUsers] = useState([]);
+
+    //当用户输入关键词或者选择select框的时候，param变
+    const [param, setParam] = useState({
+        name:"",
+        personId:""
+    });
+
+    //状态
+    const [list, setList]=useState([]);
+
+    //搜索的时候去请求数据,获取list值
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=>{
+            if(response.ok){ //请求成功
+                setList(await response.json())
+            }
+        })
+    }, [param]) //当param变化的时候去请求
+
+    //初始化users
+    useEffect(()=>{
+      fetch(`${apiUrl}/users`).then(async response=>{
+        if(response.ok){
+          setUsers(await response.json());
+        }
+      })
+    },[])
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+}
+
+
---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const SearchPanel=({users,param,setParam})=>{
+
+  return (
+    <form>
+      <div>
+          {/* 输入框，用来输入要搜索的关键词 */}
+          <input type="text" value={param.name} 
+            onChange={evt=>setParam({
+                ...param,
+                name:evt.target.value
+            })}
+          />
+
+            {/* 单选框，可以选择负责人 */}
+          <select value={param.personId} 
+            onChange={evt=>setParam({
+              ...param,
+              personId:evt.target.value
+          })}>
+
+              <option value={''}>负责人</option>
+
+              {/* 动态获取所有的负责人列表 */}
+              {
+                  users.map(user=><option key={user.id} value={user.id}>{user.name}</option>)
+              }
+          </select>
+      </div>
+    </form>
+  );
+}
+
+
