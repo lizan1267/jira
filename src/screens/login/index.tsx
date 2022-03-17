7693a28a34d1e2ab93caaf977f38b9fec6b674e1
way99 <745855589@qq.com>
2022-03-17 10:19:54 +0800

    chore:5-2 安装devtool

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,46 @@
+import React,{FormEvent} from 'react';
+
+const apiUrl=process.env.REACT_APP_API_URL;
+
+export const LoginScreen=()=>{
+
+    const login=(param:{username:string,password:string})=>{
+        //发送请求
+        fetch(`${apiUrl}/login`,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(param)
+        }).then(
+            async (response:Response)=>{
+                if(response.ok){ //请求成功
+                    
+                }
+        })
+    }
+
+    //HTMLFormElement extends Element
+    //表单提交
+    const handleSubmit=(event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();  //阻止默认提交动作
+        const username=(event.currentTarget.elements[0] as HTMLInputElement).value;//获取用户名值
+        const password=(event.currentTarget.elements[1] as HTMLInputElement).value;//获取密码值
+        login({username,password}); //调用login函数，传参数
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">用户名</label>
+                <input type="text" id={'username'} />
+            </div>
+            <div>
+                <label htmlFor="password">密码</label>
+                <input type="password" id={'password'} />
+            </div>
+            <button type={'submit'}>登录</button>
+        </form>
+    )
+}
+
